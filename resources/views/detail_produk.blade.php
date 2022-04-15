6b6d2e11c8d8f9a19d601c62e7a4a17c164b2902
Iqballlfw <iqbalfardurahman@gmail.com>
2022-04-15 21:34:36 +0700

    ini cuma nambahin layout doang, isinya boleh kalian ganti belom fix

--- resources/views/detail_produk.blade.php
+++ resources/views/detail_produk.blade.php
@@ -1,40 +1,7 @@
-<html lang="en">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Produk</title>
-</head>
+@extends('layout.template')
 
-<body>
-<!-- navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color : #964B00">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar w/ text</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-      </ul>
-      <span class="navbar-text">
-        Navbar text with an inline element
-      </span>
-    </div>
-  </div>
-</nav>
-<!-- nav -->
-
+@section('konten')
     <h1>INI halaman produk dari iqbal dan dika</h1>
+@endsection
 </body>
 </html>
