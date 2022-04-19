cd7d797748c8e5ce41ba4421ff70804d83ebd820
rizalw <rizalwidyananda@gmail.com>
2022-04-19 16:29:40 +0700

    add produk models and connection between produk - toko

---
+++ database/migrations/2022_04_19_084857_create_produks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_toko');
+            $table->string('nama');
+            $table->timestamps();
+
+            $table->foreign("id_toko")->references('id')->on('tokos')->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produks');
+    }
+};
