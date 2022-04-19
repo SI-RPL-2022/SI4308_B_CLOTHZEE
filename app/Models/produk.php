1583ac8cc1520e389df20c0321251fcbb74043e7
rizalw <rizalwidyananda@gmail.com>
2022-04-19 16:51:28 +0700

    add ukuran models for produks

--- app/Models/produk.php
+++ app/Models/produk.php
@@ -5,11 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\toko;
-
+use App\Models\ukuran;
 class produk extends Model
 {
     use HasFactory;
     public function toko(){
         return $this-> belongsTo(toko::class);
-    } 
+    }
+    public function ukuran(){
+        return $this-> hasMany(ukuran::class, 'id_produk');
+    }
 }
