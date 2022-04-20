4a1202a9c491ac05f56baeff07f4224f78eda79e
rhmfzh <rahmaananda156@gmail.com>
2022-04-20 23:45:23 +0700

    tambahan search reff #3

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,8 +1,9 @@
 @extends('layout.template')
 
 @section('konten')
-
+        <br><br><br>
         <h3 style="text-align: center;">DAFTAR KONFEKSI</h3>
+        <br><br><br>
             <div class="container-fluid" style=" height: 700px; background-color: rgb(210, 230, 216);" >
                 <div class="container">
                     <div class="row row-cols-md-4 g-4">
@@ -11,7 +12,7 @@
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="card-img-top" alt="p1" >
                                 <div class="card-body">
                                 <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
-                                <p class="card-text">Jenis kain: jersey,katun,dll, Location : Buah Batu. </p>
+                                <p class="card-text">Jenis kain: jersey,katun,dll, Location : Buah Batu </p>
                                 </div>
                             </div>
                         </div>
@@ -21,7 +22,7 @@
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="card-img-top" alt="p1">
                                 <div class="card-body">
                                 <h4 class="card-title" style="text-align: center;">AANG TAYLOR</h4>
-                                <p class="card-text">Jenis kain: jersey,katun,dll, Location : Buah Batu. </p>
+                                <p class="card-text">Jenis kain: jersey,katun,dll, Location : Buah Batu </p>
                                 </div>
                             </div>
                         </div>
@@ -31,7 +32,7 @@
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="card-img-top" alt="p1">
                                 <div class="card-body">
                                 <h4 class="card-title" style="text-align: center;">JEJOY CLOTH</h4>
-                                <p class="card-text">Jenis kain: jersey,katun,dll, Location : Buah Batu. </p>
+                                <p class="card-text">Jenis kain: jersey,katun,dll, Location : Buah Batu </p>
                                 </div>
                             </div>
                         </div>
@@ -41,20 +42,19 @@
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="card-img-top" alt="p1">
                                 <div class="card-body">
                                 <h4 class="card-title" style="text-align: center;">MBACOM'S HOUSE</h4>
-                                <p class="card-text">Jenis kain: jersey,katun,dll, Location : Buah Batu. </p>
+                                <p class="card-text">Jenis kain: jersey,katun,dll, Location : Buah Batu </p>
                                 </div>
                             </div>
                         </div>
-
                     </div>
-
+                    <br><br><br>
                     <div class="row row-cols-md-4 g-4">
                         <div class="col">
                             <div class="card">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="card-img-top" alt="p1" >
                                 <div class="card-body">
-                                <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
-                                <p class="card-text">Jenis kain: jersey,katun,dll, Location : Buah Batu. </p>
+                                <h4 class="card-title" style="text-align: center;">SEMPURNA TAYLOR</h4>
+                                <p class="card-text">Jenis kain: jersey,katun,dll, Location : Bandung </p>
                                 </div>
                             </div>
                         </div>
@@ -63,8 +63,8 @@
                             <div class="card">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="card-img-top" alt="p1">
                                 <div class="card-body">
-                                <h4 class="card-title" style="text-align: center;">AANG TAYLOR</h4>
-                                <p class="card-text">Jenis kain: jersey,katun,dll, Location : Buah Batu. </p>
+                                <h4 class="card-title" style="text-align: center;">BUNGA KONFEKSI</h4>
+                                <p class="card-text">Jenis Kain : Batik dan Seragam Location : Bandung</p>
                                 </div>
                             </div>
                         </div>
@@ -73,8 +73,8 @@
                             <div class="card">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="card-img-top" alt="p1">
                                 <div class="card-body">
-                                <h4 class="card-title" style="text-align: center;">JEJOY CLOTH</h4>
-                                <p class="card-text">Jenis kain: jersey,katun,dll, Location : Buah Batu. </p>
+                                <h4 class="card-title" style="text-align: center;">KITA HAPPY KONFEKSI</h4>
+                                <p class="card-text">Jenis Kain : Seragam Sekolah Location : Buah Batu </p>
                                 </div>
                             </div>
                         </div>
@@ -83,8 +83,8 @@
                             <div class="card">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="card-img-top" alt="p1">
                                 <div class="card-body">
-                                <h4 class="card-title" style="text-align: center;">MBACOM'S HOUSE</h4>
-                                <p class="card-text">Jenis kain: jersey,katun,dll, Location : Buah Batu. </p>
+                                <h4 class="card-title" style="text-align: center;">LOTUS CLOTH</h4>
+                                <p class="card-text">Jenis Kain : Seragam Anak Location : Buah Batu </p>
                                 </div>
                             </div>
                         </div>
