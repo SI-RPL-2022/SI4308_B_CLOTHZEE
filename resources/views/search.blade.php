3fcc5495e457df3b2bf2b9285f387ef662dff127
rhmfzh <rahmaananda156@gmail.com>
2022-04-21 00:13:58 +0700

    tambahan search ref #3

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,14 +2,15 @@
 
 @section('konten')
         <br><br><br>
-        <h3 style="text-align: center;">DAFTAR KONFEKSI</h3>
-        <br><br><br>
-            <div class="container-fluid" style=" height: 700px; background-color: rgb(210, 230, 216);" >
+        <h5 style="text-align:Left;">Menampilkan 1-10 konfeksi untuk "konfeksi di bandung"</h5>
+        <br><br>
+        <div class="rounded-3 shadow"> 
+            <div class="container-fluid" style=" height: 700px;" >
                 <div class="container">
                     <div class="row row-cols-md-4 g-4">
                         <div class="col">
                             <div class="card">
-                                <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="card-img-top" alt="p1" >
+                                <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
                                 <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
                                 <p class="card-text">Jenis kain: jersey,katun,dll, Location : Buah Batu </p>
@@ -92,5 +93,5 @@
                     </div>
                 </div>
             </div>
-
+        </div>
 @endsection
