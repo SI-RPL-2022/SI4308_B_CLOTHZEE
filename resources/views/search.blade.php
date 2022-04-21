6e6be5cc723fc67836df7dfc7a63c5899e491413
rhmfzh <rahmaananda156@gmail.com>
2022-04-21 20:47:30 +0700

    tambahan search ref #3

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,12 +4,34 @@
         <br><br><br>
         <h5 style="text-align:Left;">Menampilkan 1-10 konfeksi untuk "konfeksi di bandung"</h5>
         <br><br>
-        <div class="rounded-3 shadow"> 
-            <div class="container-fluid" style=" height: 900px;">
-                <div class="container">
+        
+            <div class="container-fluid" style=" height: 900px; ">
+                <div class="container-outline-secondary">
                     <div class="row row-cols-md-4 g-4">
                         <div class="col">
-                            <div class="card">
+                            <div class="card shadow">
+                                <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
+                                <div class="card-body">
+                                    <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">Jenis Kain : Jersey, Katun, dll</li>
+                                    <li class="list-group-item">Location : Buah Batu</li>
+                                    <li class="list-group-item">
+                                    <div class="d-flex">
+                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
+                                        <p class="pt-1">4.9 | terjual 130+</p> 
+                                    </div> 
+                                    </li>
+                                </ul>
+                                <div class="card-body">
+                                    <a href="detail_produk" class="card-link">Lihat detail</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="card shadow">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
                                     <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
@@ -25,7 +47,7 @@
                         </div>
 
                         <div class="col">
-                            <div class="card">
+                            <div class="card shadow">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
                                     <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
@@ -41,23 +63,7 @@
                         </div>
 
                         <div class="col">
-                            <div class="card">
-                                <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
-                                <div class="card-body">
-                                    <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
-                                </div>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">Jenis Kain : Jersey, Katun, dll</li>
-                                    <li class="list-group-item">Location : Buah Batu</li>
-                                </ul>
-                                <div class="card-body">
-                                    <a href="detail_produk" class="card-link">Lihat detail</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col">
-                            <div class="card">
+                            <div class="card shadow">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
                                     <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
@@ -76,7 +82,7 @@
                     <br><br>
                     <div class="row row-cols-md-4 g-4">
                         <div class="col">
-                            <div class="card">
+                            <div class="card shadow">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
                                     <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
@@ -92,7 +98,7 @@
                         </div>
 
                         <div class="col">
-                            <div class="card">
+                            <div class="card shadow">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
                                     <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
@@ -108,7 +114,7 @@
                         </div>
 
                         <div class="col">
-                            <div class="card">
+                            <div class="card shadow">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
                                     <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
@@ -124,7 +130,7 @@
                         </div>
 
                         <div class="col">
-                            <div class="card">
+                            <div class="card shadow">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
                                     <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
@@ -142,5 +148,5 @@
                         
                     </div>
                 </div>
-            </div>
+            
 @endsection
