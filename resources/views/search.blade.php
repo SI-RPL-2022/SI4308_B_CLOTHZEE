e602a82cef582e1e22f39c6475b65aa91e32166e
Firenze Sukmaning Diefta <firenzesukmaningd@gmail.com>
2022-04-21 20:58:20 +0700

    search baru ref #3

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -34,11 +34,17 @@
                             <div class="card shadow">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
-                                    <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
+                                    <h4 class="card-title" style="text-align: center;">Lotus Textile</h4>
                                 </div>
                                 <ul class="list-group list-group-flush">
                                     <li class="list-group-item">Jenis Kain : Jersey, Katun, dll</li>
                                     <li class="list-group-item">Location : Buah Batu</li>
+                                    <li class="list-group-item">
+                                    <div class="d-flex">
+                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
+                                        <p class="pt-1">4.7 | terjual 100+</p> 
+                                    </div> 
+                                    </li>
                                 </ul>
                                 <div class="card-body">
                                     <a href="detail_produk" class="card-link">Lihat detail</a>
@@ -50,11 +56,17 @@
                             <div class="card shadow">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
-                                    <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
+                                    <h4 class="card-title" style="text-align: center;">Bahagia Selalu</h4>
                                 </div>
                                 <ul class="list-group list-group-flush">
                                     <li class="list-group-item">Jenis Kain : Jersey, Katun, dll</li>
                                     <li class="list-group-item">Location : Buah Batu</li>
+                                    <li class="list-group-item">
+                                    <div class="d-flex">
+                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
+                                        <p class="pt-1">4.9 | terjual 150+</p> 
+                                    </div> 
+                                    </li>
                                 </ul>
                                 <div class="card-body">
                                     <a href="detail_produk" class="card-link">Lihat detail</a>
@@ -66,11 +78,17 @@
                             <div class="card shadow">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
-                                    <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
+                                    <h4 class="card-title" style="text-align: center;">TULUS's HOUSE</h4>
                                 </div>
                                 <ul class="list-group list-group-flush">
                                     <li class="list-group-item">Jenis Kain : Jersey, Katun, dll</li>
                                     <li class="list-group-item">Location : Buah Batu</li>
+                                    <li class="list-group-item">
+                                    <div class="d-flex">
+                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
+                                        <p class="pt-1">4.5 | terjual 80+</p> 
+                                    </div> 
+                                    </li>
                                 </ul>
                                 <div class="card-body">
                                     <a href="detail_produk" class="card-link">Lihat detail</a>
@@ -85,11 +103,17 @@
                             <div class="card shadow">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
-                                    <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
+                                    <h4 class="card-title" style="text-align: center;">Rumah Kuning</h4>
                                 </div>
                                 <ul class="list-group list-group-flush">
                                     <li class="list-group-item">Jenis Kain : Jersey, Katun, dll</li>
                                     <li class="list-group-item">Location : Buah Batu</li>
+                                    <li class="list-group-item">
+                                    <div class="d-flex">
+                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
+                                        <p class="pt-1">4.9 | terjual 180+</p> 
+                                    </div> 
+                                    </li>
                                 </ul>
                                 <div class="card-body">
                                     <a href="detail_produk" class="card-link">Lihat detail</a>
@@ -101,11 +125,17 @@
                             <div class="card shadow">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
-                                    <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
+                                    <h4 class="card-title" style="text-align: center;">Budaya Taylor</h4>
                                 </div>
                                 <ul class="list-group list-group-flush">
                                     <li class="list-group-item">Jenis Kain : Jersey, Katun, dll</li>
                                     <li class="list-group-item">Location : Buah Batu</li>
+                                    <li class="list-group-item">
+                                    <div class="d-flex">
+                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
+                                        <p class="pt-1">4.9 | terjual 150+</p> 
+                                    </div> 
+                                    </li>
                                 </ul>
                                 <div class="card-body">
                                     <a href="detail_produk" class="card-link">Lihat detail</a>
@@ -117,11 +147,17 @@
                             <div class="card shadow">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
-                                    <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
+                                    <h4 class="card-title" style="text-align: center;">KonveksiKU</h4>
                                 </div>
                                 <ul class="list-group list-group-flush">
                                     <li class="list-group-item">Jenis Kain : Jersey, Katun, dll</li>
                                     <li class="list-group-item">Location : Buah Batu</li>
+                                    <li class="list-group-item">
+                                    <div class="d-flex">
+                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
+                                        <p class="pt-1">4.8 | terjual 120+</p> 
+                                    </div> 
+                                    </li>
                                 </ul>
                                 <div class="card-body">
                                     <a href="detail_produk" class="card-link">Lihat detail</a>
@@ -133,11 +169,17 @@
                             <div class="card shadow">
                                 <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
-                                    <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
+                                    <h4 class="card-title" style="text-align: center;">Lily's</h4>
                                 </div>
                                 <ul class="list-group list-group-flush">
                                     <li class="list-group-item">Jenis Kain : Jersey, Katun, dll</li>
                                     <li class="list-group-item">Location : Buah Batu</li>
+                                    <li class="list-group-item">
+                                    <div class="d-flex">
+                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
+                                        <p class="pt-1">4.9 | terjual 150+</p> 
+                                    </div> 
+                                    </li>
                                 </ul>
                                 <div class="card-body">
                                     <a href="detail_produk" class="card-link">Lihat detail</a>
