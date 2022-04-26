fe116cc16b442a8b5a440aaded595d0c67a69555
rizalw <rizalwidyananda@gmail.com>
2022-04-27 06:38:30 +0700

    working on pemesanan (added harga model) #1

--- app/Models/ukuran.php
+++ app/Models/ukuran.php
@@ -5,11 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\produk;
+use App\Models\harga;
 
 class ukuran extends Model
 {
     use HasFactory;
     public function produk(){
         return $this-> belongsTo(produk::class);
+    }
+    public function harga(){
+        return $this->hasOne(harga::class, 'id_ukuran');
     }
 }
