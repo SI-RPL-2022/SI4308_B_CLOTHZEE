4e269d866e0a63cd277610ef697e5ece1df0c9af
rhmfzh <rahmaananda156@gmail.com>
2022-04-27 21:55:47 +0700

    semua konfeksi ref #3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,105 +23,9 @@
             </div>
         <br><br>
         
-            <!-- card rekomendasi -->
-            <div class="container-fluid" style=" height: 700px; background-color: white">
-            <h4 class="pt-3" style="text-align: left;">Rekomendasi</h4><br>
-                <div class="container-outline-secondary">
-                    <div class="row row-cols-md-4 g-4">
-                        <div class="col">
-                            <div class="card shadow">
-                                <img src="https://i.pinimg.com/originals/5c/a0/c1/5ca0c1ae50976f953d0d55c801e56945.jpg" class="rounded-3" alt="p1" >
-                                <div class="card-body">
-                                    <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
-                                </div>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">Menerima pesanan Seragam sekolah, seragam kantor, dll</li>
-                                    <li class="list-group-item">Location : Buah Batu</li>
-                                    <li class="list-group-item">
-                                    <div class="d-flex">
-                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
-                                        <p class="pt-1">4.9 | terjual 130+</p> 
-                                    </div> 
-                                    </li>
-                                </ul>
-                                <div class="card-body">
-                                    <a href="detail_produk" class="card-link">Lihat detail</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col">
-                            <div class="card shadow">
-                                <img src="https://img.freepik.com/free-vector/watercolor-andala-lotus-flower-drawing_23-2149378843.jpg?w=2000" class="rounded-3" alt="p1" >
-                                <div class="card-body">
-                                    <h4 class="card-title" style="text-align: center;">Flowers Textile</h4>
-                                </div>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">Menerima pesanan sarung, seragam sekolah, batik</li>
-                                    <li class="list-group-item">Location : Bintaro</li>
-                                    <li class="list-group-item">
-                                    <div class="d-flex">
-                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
-                                        <p class="pt-1">4.7 | terjual 100+</p> 
-                                    </div> 
-                                    </li>
-                                </ul>
-                                <div class="card-body">
-                                    <a href="detail_produk" class="card-link">Lihat detail</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col">
-                            <div class="card shadow">
-                                <img src="https://media.istockphoto.com/vectors/happy-hour-neon-sign-vector-id1018834996?k=20&m=1018834996&s=612x612&w=0&h=4NMcwNN9Y639n5CzNgNj9nS5p5OsUXj_-BFcXir87pM=" class="rounded-3" alt="p1" >
-                                <div class="card-body">
-                                    <h4 class="card-title" style="text-align: center;">HH Design</h4>
-                                </div>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">Semua jenis seragam dapat kami kerjakan dengan cepat</li>
-                                    <li class="list-group-item">Location : Sragen</li>
-                                    <li class="list-group-item">
-                                    <div class="d-flex">
-                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
-                                        <p class="pt-1">4.9 | terjual 150+</p> 
-                                    </div> 
-                                    </li>
-                                </ul>
-                                <div class="card-body">
-                                    <a href="detail_produk" class="card-link">Lihat detail</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col">
-                            <div class="card shadow">
-                                <img src="https://assets.promediateknologi.com/crop/0x0:0x0/x/photo/2022/03/09/1186417198.jpeg" class="rounded-3" alt="p1" >
-                                <div class="card-body">
-                                    <h4 class="card-title" style="text-align: center;">Rumah Manusia</h4>
-                                </div>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">Menerima pesanan batik dalam jumlah yang besar</li>
-                                    <li class="list-group-item">Location : Pekalongan</li>
-                                    <li class="list-group-item">
-                                    <div class="d-flex">
-                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
-                                        <p class="pt-1">4.6 | terjual 80+</p> 
-                                    </div> 
-                                    </li>
-                                </ul>
-                                <div class="card-body">
-                                    <a href="detail_produk" class="card-link">Lihat detail</a>
-                                </div>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                </div>
-                <br><br>
                  <!-- card konfeksi berdasarkan abjad -->
-                 <div class="container-fluid" style=" height: 700px; background-color: white">
-            <h4 class="pt-3" style="text-align: left;">Berdasarkan Abjad</h4><br>
+            <div class="container-fluid" style=" height: 1400px; background-color: white">
+            <h4 class="pt-3" style="text-align: left;">Semua Konfeksi</h4><br>
                 <div class="container-outline-secondary">
                     <div class="row row-cols-md-4 g-4">
                         <div class="col">
@@ -212,7 +116,98 @@
                             </div>
                         </div>
                         </div>
+                        <br><br>
+                        <div class="row row-cols-md-4 g-4">
+                        <div class="col">
+                            <div class="card shadow">
+                                <img src="https://img.freepik.com/free-vector/watercolor-andala-lotus-flower-drawing_23-2149378843.jpg?w=2000" class="rounded-3" alt="p1" >
+                                <div class="card-body">
+                                    <h4 class="card-title" style="text-align: center;">Flowers Textile</h4>
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">Menerima pesanan sarung, seragam sekolah, batik</li>
+                                    <li class="list-group-item">Location : Bintaro</li>
+                                    <li class="list-group-item">
+                                    <div class="d-flex">
+                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
+                                        <p class="pt-1">4.7 | terjual 100+</p> 
+                                    </div> 
+                                    </li>
+                                </ul>
+                                <div class="card-body">
+                                    <a href="detail_produk" class="card-link">Lihat detail</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="card shadow">
+                                <img src="https://media.istockphoto.com/vectors/happy-hour-neon-sign-vector-id1018834996?k=20&m=1018834996&s=612x612&w=0&h=4NMcwNN9Y639n5CzNgNj9nS5p5OsUXj_-BFcXir87pM=" class="rounded-3" alt="p1" >
+                                <div class="card-body">
+                                    <h4 class="card-title" style="text-align: center;">HH Design</h4>
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">Semua jenis seragam dapat kami kerjakan dengan cepat</li>
+                                    <li class="list-group-item">Location : Sragen</li>
+                                    <li class="list-group-item">
+                                    <div class="d-flex">
+                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
+                                        <p class="pt-1">4.9 | terjual 150+</p> 
+                                    </div> 
+                                    </li>
+                                </ul>
+                                <div class="card-body">
+                                    <a href="detail_produk" class="card-link">Lihat detail</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="card shadow">
+                                <img src="https://assets.promediateknologi.com/crop/0x0:0x0/x/photo/2022/03/09/1186417198.jpeg" class="rounded-3" alt="p1" >
+                                <div class="card-body">
+                                    <h4 class="card-title" style="text-align: center;">Rumah Manusia</h4>
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">Menerima pesanan batik dalam jumlah yang besar</li>
+                                    <li class="list-group-item">Location : Pekalongan</li>
+                                    <li class="list-group-item">
+                                    <div class="d-flex">
+                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
+                                        <p class="pt-1">4.6 | terjual 80+</p> 
+                                    </div> 
+                                    </li>
+                                </ul>
+                                <div class="card-body">
+                                    <a href="detail_produk" class="card-link">Lihat detail</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="card shadow">
+                                <img src="https://i.pinimg.com/originals/5c/a0/c1/5ca0c1ae50976f953d0d55c801e56945.jpg" class="rounded-3" alt="p1" >
+                                <div class="card-body">
+                                    <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">Menerima pesanan Seragam sekolah, seragam kantor, dll</li>
+                                    <li class="list-group-item">Location : Buah Batu</li>
+                                    <li class="list-group-item">
+                                    <div class="d-flex">
+                                        <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
+                                        <p class="pt-1">4.9 | terjual 130+</p> 
+                                    </div> 
+                                    </li>
+                                </ul>
+                                <div class="card-body">
+                                    <a href="detail_produk" class="card-link">Lihat detail</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        </div>
                     </div>
                 </div>
-                <br><br>
+                <br><br><br><br>
 @endsection
