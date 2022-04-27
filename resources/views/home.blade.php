9f0cc87be9151e51d5838fdde479a40062a910aa
Firenze Sukmaning Diefta <firenzesukmaningd@gmail.com>
2022-04-27 11:40:40 +0700

    details home ref #3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,12 +28,12 @@
                     <div class="row row-cols-md-4 g-4">
                         <div class="col">
                             <div class="card shadow">
-                                <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
+                                <img src="https://i.pinimg.com/originals/5c/a0/c1/5ca0c1ae50976f953d0d55c801e56945.jpg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
                                     <h4 class="card-title" style="text-align: center;">UJANG KONFEKSI</h4>
                                 </div>
                                 <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">Jenis Kain : Jersey, Katun, dll</li>
+                                    <li class="list-group-item">Menerima pesanan Seragam sekolah, seragam kantor, dll</li>
                                     <li class="list-group-item">Location : Buah Batu</li>
                                     <li class="list-group-item">
                                     <div class="d-flex">
@@ -50,13 +50,13 @@
 
                         <div class="col">
                             <div class="card shadow">
-                                <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
+                                <img src="https://img.freepik.com/free-vector/watercolor-andala-lotus-flower-drawing_23-2149378843.jpg?w=2000" class="rounded-3" alt="p1" >
                                 <div class="card-body">
                                     <h4 class="card-title" style="text-align: center;">Lotus Textile</h4>
                                 </div>
                                 <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">Jenis Kain : Jersey, Katun, dll</li>
-                                    <li class="list-group-item">Location : Buah Batu</li>
+                                    <li class="list-group-item">Menerima pesanan sarung, seragam sekolah, batik</li>
+                                    <li class="list-group-item">Location : Bintaro</li>
                                     <li class="list-group-item">
                                     <div class="d-flex">
                                         <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
@@ -72,13 +72,13 @@
 
                         <div class="col">
                             <div class="card shadow">
-                                <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
+                                <img src="https://media.istockphoto.com/vectors/happy-hour-neon-sign-vector-id1018834996?k=20&m=1018834996&s=612x612&w=0&h=4NMcwNN9Y639n5CzNgNj9nS5p5OsUXj_-BFcXir87pM=" class="rounded-3" alt="p1" >
                                 <div class="card-body">
                                     <h4 class="card-title" style="text-align: center;">Bahagia Selalu</h4>
                                 </div>
                                 <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">Jenis Kain : Jersey, Katun, dll</li>
-                                    <li class="list-group-item">Location : Buah Batu</li>
+                                    <li class="list-group-item">Semua jenis seragam dapat kami kerjakan dengan cepat</li>
+                                    <li class="list-group-item">Location : Sragen</li>
                                     <li class="list-group-item">
                                     <div class="d-flex">
                                         <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
@@ -94,13 +94,13 @@
 
                         <div class="col">
                             <div class="card shadow">
-                                <img src="http://www.bastelindo.com/wp-content/uploads/2019/01/sejarah-konveksi-bandung-e1547664632904.jpeg" class="rounded-3" alt="p1" >
+                                <img src="https://assets.promediateknologi.com/crop/0x0:0x0/x/photo/2022/03/09/1186417198.jpeg" class="rounded-3" alt="p1" >
                                 <div class="card-body">
                                     <h4 class="card-title" style="text-align: center;">TULUS's HOUSE</h4>
                                 </div>
                                 <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">Jenis Kain : Jersey, Katun, dll</li>
-                                    <li class="list-group-item">Location : Buah Batu</li>
+                                    <li class="list-group-item">Menerima pesanan batik dalam jumlah yang besar</li>
+                                    <li class="list-group-item">Location : Pekalongan</li>
                                     <li class="list-group-item">
                                     <div class="d-flex">
                                         <img src="img/logo/star.png" style="width: 20px; height:20px;" alt="" class="m-1">
