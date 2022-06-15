e06f275cd0e026b2d065f616c3237e615a4ad95a
rhmfzh <rahmaananda156@gmail.com>
2022-06-15 13:25:19 +0700

    edit produk kedua ref #11

--- resources/views/edit_produk.blade.php
+++ resources/views/edit_produk.blade.php
@@ -23,6 +23,7 @@
 </style>
 <br>
 <!-- row pertama -->
+<div class="container-fluid">
 <h2 class="text-center"><b>Edit Produk</b></h2>
 <hr>
 <div class="row mt-4">
@@ -30,22 +31,22 @@
     <div class="col-7">
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label"><b>Nama Produk</b></label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Contoh : Tampan Tailor">
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Contoh : Seragam Sekolah">
         </div>
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label"><b>Gambar Produk</b></label>
-            <input type="file" class="form-control" id="exampleFormControlInput1" placeholder="Contoh : Tampan Tailor">
+            <input type="file" class="form-control" id="exampleFormControlInput1" placeholder="">
         </div>
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label"><b>Social Media (Instagram)</b></label>
+            <label for="exampleFormControlInput1" class="form-label"><b>zal aku gatau isinya apa jadi ni aku tetep kasih tempat hehe</b></label>
             <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Kosongkan bila tidak ada">
         </div>
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label"><b>Social Media (Whatsapp)</b></label>
+            <label for="exampleFormControlInput1" class="form-label"><b>zal aku gatau isinya apa jadi ni aku tetep kasih tempat hehe</b></label>
             <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Kosongkan bila tidak ada">
         </div>
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label"><b>Lokasi</b></label>
+            <label for="exampleFormControlInput1" class="form-label"><b>zal aku gatau isinya apa jadi ni aku tetep kasih tempat hehe</b></label>
             <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Masukan Link Embed">
         </div>
         <div class="mb-3">
@@ -57,6 +58,7 @@
         </div>
     </div>
 
+</div>
 </div>
 <br><br>
 @endsection
